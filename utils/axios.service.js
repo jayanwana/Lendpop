140218fa2ed63fd38b3e0fe87cc96ded31e98e83
Aj <jayanwana@gmail.com>
2020-06-07 15:22:54 +0100

    update axios

--- utils/axios.service.js
+++ utils/axios.service.js
@@ -12,6 +12,10 @@ class LendPopApi {
   verification(data) {
     return axios.post("/email", data)
   }
+
+  password(data) {
+    return axios.post("/password", data)
+  }
 }
 
 export default new LendPopApi();
