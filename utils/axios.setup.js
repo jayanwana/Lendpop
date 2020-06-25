8a6bdd66ab8060a0d86133dc04bdcc4f23a5cf6d
Simoncods <admin@uncensoredwears.com>
2020-06-25 13:15:42 +0100

    commit on 'deploy'

--- utils/axios.setup.js
+++ utils/axios.setup.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const source = axios.CancelToken.source
 
 export default axios.create({
-  baseURL: "https://api.instakash.co",
+  baseURL: "https://api.instakash.co/api",
   headers: {
     "Content-Type": "application/json",
     "Authorization": "Basic YWRtaW46YWRtaW4="
